ba6a4e0918564a426186d955c1cfd47bbdd85470
zachary-loughridge <60743188+zachary-loughridge@users.noreply.github.com>
2020-02-06 08:50:08 -0500

    Create User.java

---
+++ enrollments/pojos/User.java
@@ -0,0 +1,17 @@
+package enrollments.pojos;
+
+public class User {
+	//header row indexes
+	public int userIdIndex;
+	public int firstNameIndex;
+	public int lastNameIndex;
+	public int versionIndex;
+	public int insuranceCompanyIndex;
+	
+	//user properties
+	public String userId = "";
+	public String firstName = "";
+	public String lastName = "";
+	public int version = 0;
+	public String insuranceCompany = "";
+}
