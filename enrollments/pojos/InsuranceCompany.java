8b10cf0e4a075beebb9477e1d2f5fd3de94c45c4
zachary-loughridge <60743188+zachary-loughridge@users.noreply.github.com>
2020-02-10 08:13:19 -0500

    Create InsuranceCompany.java

---
+++ enrollments/pojos/InsuranceCompany.java
@@ -0,0 +1,9 @@
+package enrollments.pojos;
+import java.util.ArrayList;
+import enrollments.pojos.User;
+
+public class InsuranceCompany {
+	public String insuranceName;
+	public String fileName;
+	public ArrayList<User> users = new ArrayList<User>();
+}
